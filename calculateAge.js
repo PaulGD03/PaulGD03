8e0174655bc22fb2cfb406a9b318ccdb723081f2
Paul_GD <paulskapczyk@gmail.com>
2024-10-30 18:16:57 +0100

    Update calculateAge.js

--- calculateAge.js
+++ calculateAge.js
@@ -16,32 +16,34 @@ function getAge(birthdate) {
 }
 
 // Generate the new README content
-const readmeContent = `# About Me
+const readmeContent = `# # 👋 Hi, I'm Paul!
 
-I am currently ${getAge(birthdate)} years old. I am a Mechatronics Student (B.Eng.) and the lead for mechatronics at TheContrappostoShop, and part of the leadership of the Open Resin Alliance. I am a huge open-source advocate and have been involved with 3D printing since 2018.
+I am currently [AGE_PLACEHOLDER] years old.
 
-## Proficient in
-- Flutter
-- Dart
-- C
-- Java
-- C#
-- C++
-- JavaScript
+I am a Mechatronics Student (B.Eng.) and the lead for mechatronics at TheContrappostoShop,<br>and part of the leadership of the Open Resin Alliance.
 
-## Connect with Me
+I am a huge open-source advocate and have been involved with 3D printing since 2018.
+
+## 🛠️ Tech Stack
+- **Proficient in**: 
+  - 🐦 Flutter
+  - 🎯 Dart
+  - 💻 C
+  - ☕ Java
+  - 🎨 C#
+  - ➕ C++
+  - 🌐 JavaScript
+
+## 🌐 Connect with Me
 - **Twitter**: [Paul_GD03](https://twitter.com/Paul_GD03)
 - **Discord**: [@paulgd](https://discord.com/)
 - **Email**: [paul@openresin.org](mailto:paul@openresin.org)
 
-## GitHub Stats
-![GitHub Stats](https://github-readme-stats.vercel.app/api?username=PaulGD03&theme=dark&hide_border=true&include_all_commits=true&count_private=true)
-![GitHub Streak](https://github-readme-streak-stats.herokuapp.com/?user=PaulGD03&theme=dark&hide_border=true)
-
-## Support Me
+## ☕ Support Me
 [![Ko-Fi](https://img.shields.io/badge/Ko--fi-F16061?style=for-the-badge&logo=ko-fi&logoColor=white)](https://ko-fi.com/paul_gd)
 
 <!-- Proudly created with GPRM ( https://gprm.itsvg.in ) -->
+
 `;
 
 // Write the new README content to the README.md file
